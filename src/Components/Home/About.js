2f52e39ddcbc1b4e0037d3c4e4f5078937661a0b
SumiAkter2 <akteralisha36@gmail.com>
2023-01-11 18:57:27 +0600

    added about

---
+++ src/Components/Home/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+import img from "../asset/depositphotos_202173158-stock-photo-male-hands-holding-power-drill.jpg";
+const About = () => {
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200 px-12">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={img} className="w-2/5 rounded-lg shadow-2xl" />
+          <div className="text-left">
+            <h1 className="text-5xl font-bold text-pink-400">
+              Something About Us
+            </h1>
+            <div className="divider "></div>
+            <p className="py-4">
+              We provide best tools for our clients that they can benefited. the
+              demand of users all over to have more access to content. Between
+              driller, nuts, bolt, and accessibility on the tools is so easy to
+              use.
+            </p>
+            <p className="text-xl mb-6">We ensure our products :</p>
+
+            <p>Hard, stainless steel, customizable.</p>
+            <p>High bran electric ware, strong parts.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Reviews from "../Reviews/Reviews";
+import About from "./About";
 import Banner from "./Banner";
 import GetReview from "./GetReview";
 import Products from "./Products";
-
+import Summary from './Summary'
 const Home = () => {
   return (
     <div>
       <Banner />
           <Products />
           <GetReview />
-          <Reviews/>
+          <Reviews />
+          <Summary />
+          <About
     </div>
   );
 };
